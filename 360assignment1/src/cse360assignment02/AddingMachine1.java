69a9698188b2cac6db52eb94e10bb0d47c133833
megan <megan@SKUTT-HP>
2020-10-09 17:19:15 -0700

    First Adding Machine

---
+++ 360assignment1/src/cse360assignment02/AddingMachine1.java
@@ -0,0 +1,26 @@
+package cse360assignment02;
+
+public class AddingMachine1 {
+	  private int total;
+		  
+	  public AddingMachine1() {
+		  total = 0;  // not needed - included for clarity
+	  }
+		  
+	  public int getTotal () { // get total
+		  return 0;
+	  }
+		  
+	  public void add (int value) { // adding function
+	  }
+
+	  public void subtract (int value) { // subtraction function
+	  }
+
+	  public String toString () { // get the string
+		  return "";
+	  }
+
+	  public void clear() { // reset
+	  }
+}
